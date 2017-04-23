407f12ccffaf2d0db8dc2f18e736520bf8a07b6a
Zackery Rilling <rillingz@yahoo.com>
2017-04-23 15:13:32 -0700

    sql db created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO `burgers` (burger_name) VALUES ('DOUBLE DOUBLE ANIMAL STYLE');
+INSERT INTO `burgers` (burger_name) VALUES ('BACON BURGER');
+INSERT INTO `burgers` (burger_name) VALUES ('MAKERS MARK BURGER');
